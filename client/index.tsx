40049b14c608bc6cffa8736a6f44e47b3affed35
pnlybubbles <pnlybubbles@gmail.com>
2017-10-16 00:23:54 +0900

    feat: introduce mobx

--- client/index.tsx
+++ client/index.tsx
@@ -1,11 +1,32 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
+import { observable, action } from "mobx"
 
-const Hello: React.SFC = () => {
-  return <h1>Hello World!</h1>
+class Ping extends React.PureComponent<{ store: Store }> {
+  render() {
+    return (
+      <div>
+        <button onClick={this.props.store.ping}>ping</button>
+        <h1>{this.props.store.message}</h1>
+      </div>
+    )
+  }
+}
+
+type PingResponse = {
+  message: string
+}
+
+class Store {
+  @observable message: string = 'no data'
+
+  @action async ping() {
+    const res = await fetch('/api/ping')
+    this.message = (await res.json() as PingResponse).message
+  }
 }
 
 ReactDOM.render(
-  <Hello />,
+  <Ping store={new Store()} />,
   document.body
 )
